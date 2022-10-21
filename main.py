b864b02a868160f62fb68b2fb8a007988d276481
iBizzyNew <94970740+iBizzyNew@users.noreply.github.com>
2022-10-21 12:59:54 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-a = input("Введите число: ")
-even = 0
-odd = 0
-for i in a:
-    if int(i) % 2 == 0:
-        even += 1
-    else:
-        odd += 1
-print("Четных: %d, Не четных: %d" % (even, odd))
